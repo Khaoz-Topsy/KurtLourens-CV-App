4cb93a6af3e884182f12b0cec51bc1e0d93e431a
Kurt Lourens <hi@kurtlourens.com>
2022-08-31 15:49:04 +0200

    💄 UI tweaks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ import '../components/adaptive/homepage_appbar.dart';
 import '../components/common/cached_future_builder.dart';
 import '../components/drawer.dart';
 import '../components/scaffoldTemplates/generic_page_scaffold.dart';
+import '../components/tilePreseneters/blog_post_tile_presenter.dart';
 import '../components/tilePreseneters/project_experience_tile_presenter.dart';
 import '../components/tilePreseneters/work_experience_tile_presenter.dart';
 import '../constants/app_image.dart';
@@ -31,31 +32,31 @@ class Homepage extends StatelessWidget {
         whileLoading: const Center(),
         whenDoneLoading: (ResultWithValue<CvData> result) {
           return animateWidgetIn(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-              child: BreakpointBuilder(
-                builder: (BuildContext innerContext, Breakpoint breakpoint) {
-                  int numCols = getHomepageColumnCount(breakpoint);
-                  List<StaggeredGridItem> gridItems = getGridItems(
-                    innerContext,
-                    result.value,
-                    drawerIconColour,
-                    numCols,
-                  );
+            child: BreakpointBuilder(
+              builder: (BuildContext innerContext, Breakpoint breakpoint) {
+                int numCols = getHomepageColumnCount(breakpoint);
+                double blogPostHeight =
+                    getHomepageBlogPostColumnHeight(breakpoint);
+                List<StaggeredGridItem> gridItems = getGridItems(
+                  innerContext,
+                  result.value,
+                  drawerIconColour,
+                  blogPostHeight,
+                  numCols,
+                );
 
-                  return StaggeredGridView.countBuilder(
-                    key: Key("staggeredGrid-col-$numCols"),
-                    crossAxisCount: numCols,
-                    itemCount: gridItems.length,
-                    itemBuilder: (BuildContext itemContext, int index) =>
-                        gridItems[index].childBuilder!(itemContext),
-                    staggeredTileBuilder: (int index) =>
-                        gridItems[index].gridItemType,
-                    mainAxisSpacing: 4.0,
-                    crossAxisSpacing: 4.0,
-                  );
-                },
-              ),
+                return StaggeredGridView.countBuilder(
+                  key: Key("staggeredGrid-col-$numCols"),
+                  crossAxisCount: numCols,
+                  itemCount: gridItems.length,
+                  itemBuilder: (BuildContext itemContext, int index) =>
+                      gridItems[index].childBuilder!(itemContext),
+                  staggeredTileBuilder: (int index) =>
+                      gridItems[index].gridItemType,
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                );
+              },
             ),
           );
         },
@@ -68,54 +69,61 @@ List<StaggeredGridItem> getGridItems(
   BuildContext innerContext,
   CvData cvData,
   Color drawerIconColour,
+  double blogPostHeight,
   int numCols,
 ) =>
     [
       StaggeredGridItem(
-        childBuilder: (BuildContext childContext) => Card(
-          margin: const EdgeInsets.only(top: 16.0, left: 4.0, right: 4.0),
-          child: Row(
-            children: [
-              localImage(
-                AppImage.avatar,
-                boxfit: BoxFit.fitHeight,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(4),
-                  bottomLeft: Radius.circular(4),
-                ),
-              ),
-              emptySpace(2),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'Kurt Lourens',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 20),
+        childBuilder: (BuildContext childContext) => Padding(
+          padding: const EdgeInsets.only(left: 12.0, right: 2.0),
+          child: Card(
+            margin: const EdgeInsets.only(top: 16.0),
+            child: Row(
+              children: [
+                localImage(
+                  AppImage.avatar,
+                  boxfit: BoxFit.fitHeight,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    bottomLeft: Radius.circular(4),
                   ),
-                  Text(
-                    'Senior Software Engineer',
-                    maxLines: 2,
-                  )
-                ],
-              )
-            ],
+                ),
+                emptySpace(2),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Kurt Lourens',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Senior Software Engineer',
+                      maxLines: 2,
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
         gridItemType: StaggeredTile.count((numCols - 2), 1.25),
       ),
       StaggeredGridItem(
-        childBuilder: (BuildContext childContext) => Card(
-          margin: const EdgeInsets.only(top: 16.0, left: 4.0, right: 4.0),
-          child: InkWell(
-            onTap: () => getNavigation().navigateAwayFromHomeAsync(
-              childContext,
-              navigateToNamed: Routes.about,
-            ),
-            child: const Center(
-              child: Icon(Icons.question_mark),
+        childBuilder: (BuildContext childContext) => Padding(
+          padding: const EdgeInsets.only(left: 2.0, right: 12.0),
+          child: Card(
+            margin: const EdgeInsets.only(top: 16.0),
+            child: InkWell(
+              onTap: () => getNavigation().navigateAwayFromHomeAsync(
+                childContext,
+                navigateToNamed: Routes.about,
+              ),
+              child: const Center(
+                child: Icon(Icons.question_mark),
+              ),
             ),
           ),
         ),
@@ -144,6 +152,20 @@ List<StaggeredGridItem> getGridItems(
                 cvData.tech,
               ),
               gridItemType: const StaggeredTile.count(8, 1.3),
+            ),
+          )
+          .toList()),
+      //
+      sectionHeading('Latest Blog posts', numCols),
+      ...(cvData.blogPosts
+          .map(
+            (blogPost) => StaggeredGridItem(
+              childBuilder: (BuildContext childContext) =>
+                  blogPostGridTilePresenter(
+                childContext,
+                blogPost,
+              ),
+              gridItemType: StaggeredTile.count(8, blogPostHeight),
             ),
           )
           .toList()),
